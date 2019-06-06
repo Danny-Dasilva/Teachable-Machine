1a7a4e132fca152f49f3e72936a6e7ea27b41d53
tech-garage <yahchayildasilva@gmail.com>
2019-06-06 13:00:35 -0400

    camera stream

--- streaming/gstreamer/server.py
+++ streaming/gstreamer/server.py
@@ -1,5 +1,20 @@
 from classify import streaming
 
+# import required modules
+from flask import Flask, render_template, Response 
 
-if __name__ == '__main__':
-    streaming()
+import socket 
+import io 
+app = Flask(__name__) 
+@app.route('/') 
+def index(): 
+   """Video streaming .""" 
+   return render_template('index.html') 
+
+@app.route('/video_feed') 
+def video_feed(): 
+   """Video streaming route. Put this in the src attribute of an img tag.""" 
+   return Response(streaming(), 
+                   mimetype='multipart/x-mixed-replace; boundary=frame') 
+if __name__ == '__main__': 
+	app.run(host='0.0.0.0', debug=True, threaded=True) 
