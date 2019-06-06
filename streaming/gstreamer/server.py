08282e329c87534c847790434412bcd31a481b54
tech-garage <yahchayildasilva@gmail.com>
2019-06-06 13:25:31 -0400

    check if its ctually running on the port

--- streaming/gstreamer/server.py
+++ streaming/gstreamer/server.py
@@ -1,20 +1,23 @@
 from classify import streaming
 
-# import required modules
-from flask import Flask, render_template, Response 
+# # import required modules
+# from flask import Flask, render_template, Response 
 
-import socket 
-import io 
-app = Flask(__name__) 
+# import socket 
+# import io 
+# app = Flask(__name__) 
 
-@app.route('/') 
-def index(): 
-   """Video streaming .""" 
-   return render_template('index.html') 
+# @app.route('/') 
+# def index(): 
+#    """Video streaming .""" 
+#    return render_template('index.html') 
 
-@app.route('/video_feed') 
-def video_feed(): 
-   """Video streaming route. Put this in the src attribute of an img tag.""" 
-   return streaming()
+# @app.route('/video_feed') 
+# def video_feed(): 
+#    """Video streaming route. Put this in the src attribute of an img tag.""" 
+#    return streaming()
+# if __name__ == '__main__': 
+# 	app.run(host='0.0.0.0', debug=True, threaded=True) 
+
 if __name__ == '__main__': 
-	app.run(host='0.0.0.0', debug=True, threaded=True) 
+    streaming()
