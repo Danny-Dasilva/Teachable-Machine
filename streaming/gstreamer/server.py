8beb668e8cfeeda8b321d67a08f337c233ff9e2b
tech-garage <yahchayildasilva@gmail.com>
2019-06-06 13:16:32 -0400

    streaming sintax change

--- streaming/gstreamer/server.py
+++ streaming/gstreamer/server.py
@@ -15,7 +15,6 @@ def index():
 @app.route('/video_feed') 
 def video_feed(): 
    """Video streaming route. Put this in the src attribute of an img tag.""" 
-   return Response(streaming(), 
-                   mimetype='multipart/x-mixed-replace; boundary=frame') 
+   return Response(streaming()) 
 if __name__ == '__main__': 
 	app.run(host='0.0.0.0', debug=True, threaded=True) 
