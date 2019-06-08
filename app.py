568aa4694bac5de8d429846ce8b2cc5a2cd4be14
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-06-07 23:43:26 -0400

    process thread

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from teachable import main
 import sys
-from flask import Flask, render_template
 from multiprocessing import Process
 import time
 from Cam.apps import run_server
 from Cam.classify import render_gen
 
-app = Flask(__name__)
 
 def f():
     sys.exit(main(sys.argv))
@@ -15,21 +13,14 @@ def f():
 def l():
     run_server(render_gen)
 
-@app.route('/server')
-def start():
-    global p
-    p = Process(target=f)
-    p.start()
-    return render_template('layout.html')
-
-@app.route('/are')
-def llll():
-    global l
-    l = Process(target=l)
-    l.start()
-    return render_template('layout.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    global p
+    p = Process(target=f)
+    p.start()
+    global l
+    l = Process(target=l)
+    l.start()
+    
 
