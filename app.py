fc7aff69db06c8649670dc0cd122d7e1dc502e71
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-06-07 23:38:26 -0400

    EE

--- app.py
+++ app.py
@@ -6,18 +6,28 @@ import time
 from Cam.apps import run_server
 from Cam.classify import render_gen
 
-app = Flask(__name__)
+# app = Flask(__name__)
+
+# def f():
+#     sys.exit(main(sys.argv))
+
+# @app.route('/')
+# def start():
+#     global p
+#     p = Process(target=f)
+#     p.start()
+#     return render_template('layout.html')
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=True)
+
 
 def f():
     run_server(render_gen)
 
-@app.route('/')
-def start():
+  
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
     global p
     p = Process(target=f)
     p.start()
-    return render_template('layout.html')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-
